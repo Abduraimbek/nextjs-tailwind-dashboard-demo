5fc6b44e59831fe00f29151926a99d3413848b61
Abduraimbek <yarkinovabduraimbek@gmail.com>
2023-07-30 15:27:39 +0300

    initial

---
+++ components/BarChart.jsx
@@ -0,0 +1,64 @@
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { useEffect, useState } from 'react';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function BarChart() {
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      datasets: [
+        {
+          label: 'Sales $',
+          data: [18245, 12052, 22050, 14501, 23502, 22511, 15000],
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgb(53, 162, 235, 0.4)',
+        },
+      ],
+    });
+
+    setChartOptions({
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Daily Revenue',
+        },
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <div className='w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
+}
